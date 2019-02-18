c9ece5a8fbbf544404fa89b4de7f1fab961eb59d
Jeongeun Lee <jelee@lxplus047.cern.ch>
2019-02-18 09:22:38 +0100

    add vprime_nlo_ufo.tgz url

--- bin/MadGraph5_aMCatNLO/gridpack_generation.sh
+++ bin/MadGraph5_aMCatNLO/gridpack_generation.sh
@@ -193,7 +193,8 @@ make_gridpack () {
           #get needed BSM model
           if [[ $model = *[!\ ]* ]]; then
             echo "Loading extra model $model"
-            wget --no-check-certificate https://cms-project-generators.web.cern.ch/cms-project-generators/$model	
+            wget --no-check-certificate https://cms-project-generators.web.cern.ch/cms-project-generators/$model
+            wget --no-check-certificate http://feynrules.irmp.ucl.ac.be/raw-attachment/wiki/WZPrimeAtNLO/$model
             cd models
             if [[ $model == *".zip"* ]]; then
               unzip ../$model
