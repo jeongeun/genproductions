139f87b4f7c9bbfd2b01395b568271e060d3243d
Jeongeun Lee <jeongeun.lee@cern.ch>
2019-02-19 16:53:58 +0100

    add WprimeToENu_NLO cff python file

---
+++ python/ThirteenTeV/WprimeToLNu_NLO/WprimeToENu_M-1000_NNPDF31nnlo_TuneCP5_13TeV_madgraph-pythia8_cff.py
@@ -0,0 +1,41 @@
+import FWCore.ParameterSet.Config as cms
+
+externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
+    args = cms.vstring('/x3/cms/jelee/MCProd/mg26X/gridpacks/WprimeToENu_M1000_NLO_NNPDF31nnlo_slc6_amd64_gcc630_CMSSW_9_3_8_tarball.tar.xz'),
+    nEvents = cms.untracked.uint32(20000),
+    numberOfParameters = cms.uint32(1),
+    outputFile = cms.string('cmsgrid_final.lhe'),
+    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
+)
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+
+###Included for 2018
+from Configuration.Generator.PSweightsPythia.PythiaPSweightsSettings_cfi import *
+
+generator = cms.EDFilter("Pythia8HadronizerFilter",
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13000.),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8CP5SettingsBlock,
+        pythia8PSweightsSettingsBlock,
+        processParameters = cms.vstring(
+            'PDF:pSet = LHAPDF6:NNPDF31_nnlo_as_0118_nf_4',
+            'NewGaugeBoson:ffbar2Wprime = on',
+            '34:m0 = 1000',
+            '34:onMode = off',
+            '34:onIfAny = 11,12',
+            ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8PSweightsSettings',
+                                    'pythia8CP5Settings',
+                                    )
+    )
+)
+
+ProductionFilterSequence = cms.Sequence(generator)
+
